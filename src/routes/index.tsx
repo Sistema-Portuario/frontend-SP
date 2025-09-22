0dabdcf038de95d00f387ae6626eeb0f3acc1549
unknown <andressaconstantinooficiall@gmail.com>
2025-09-22 19:12:57 -0300

    add-login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import AdminDashboard from '../pages/AdminDashboard';
+import Login from '../pages/login';
 
 export const RoutesApp = () => {
   return (
     <Routes>
       <Route path="/" element={<AdminDashboard />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
